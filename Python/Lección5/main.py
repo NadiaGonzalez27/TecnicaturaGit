1b4a09fcdf0277687ca1e3e635e29c73c83f93b1
Nadia Gonzalez <nadiagonzalez206@gmail.com>
2022-10-22 21:02:36 -0300

    Clase de Funciones en Python y ejercicios
    Los ejercicios son del 3 al 5

--- Python/Lección5/main.py
+++ Python/Lección5/main.py
@@ -69,3 +69,31 @@ def listaNombres(*nombres): # Normalmente se utiliza: *args
         print(nombre)
 listaNombres('Lucas', 'José', 'Claudia', 'Rosa', 'Maria')
 listaNombres('Marcos', 'Daniel', 'Romina', 'Pepe', 'Marcela', 'Carlos')
+
+def listarTerminos(**terminos): # Lo mas utilizado es **kwargs para recibir los argumentos
+    for llave, valor in terminos.items(): # kwargs significa: key word argument
+        print(f'{llave} : {valor}')
+listarTerminos() # No recibe nada, nada se va a mostrar
+listarTerminos(IDE='Integrated Develoment Enviroment', PK='Primaruy Key')
+listarTerminos(Nombre='Leonel Messi')
+
+def desplegarNombres(nombres):
+    for nombre in nombres:
+        print(nombre)
+nombres2 = ['Tito', 'Pedro', 'Carlos']
+desplegarNombres(nombres2)
+desplegarNombres('Carla')
+# desplegarNombres(10, 11) # No es un objeto iterable
+desplegarNombres((10, 11)) # La convertimos a una tupla, en un solo elemento no olvidar la coma
+desplegarNombres([22, 55]) # La convertimos en una lista
+
+# Funciones Recursivas
+def factorial(numero):
+    if numero == 1: # Caso Base
+        return 1
+    else:
+        return numero * factorial(numero-1) # Caso Recursivo
+numeroFactorial = int(input("Digite el numero para calcular el factorial: "))
+resultado = factorial(numeroFactorial) # Lo hacemos en codigo duro
+print(f'El factorial del numero {numeroFactorial} es: {resultado}')
+# Tarea que el usuario ingrese el numero para calcular el factorial
