2e25a45b38e8638ee87831e53248bfff23839452
Nadia Gonzalez <nadiagonzalez206@gmail.com>
2023-04-12 11:11:39 -0300

    Agregamos al repositorio clases de JavaScript

---
+++ Javascript/leccion02/02-01-TiposDatos.js
@@ -0,0 +1,20 @@
+// Tipos de Datos en JavaScript
+/*
+La sintaxis en lo que es comentarios
+es muy similar a la de Java
+realmente diriamos que es identica
+*/
+
+var nombre = 'Ariel'; //Tipo Str
+console.log(nombre);
+
+var numero = 3000; //Tipo Númerico
+console.log(numero); 
+
+var objeto = {
+    nombre : "Nadia",
+    apellido : "Gonzalez",
+    telefono : 2604205460
+}
+
+console.log(objeto);
