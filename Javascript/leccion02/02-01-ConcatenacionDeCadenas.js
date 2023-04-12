038c81170474c52ba639c8900b547f60794423b9
Nadia Gonzalez <nadiagonzalez206@gmail.com>
2023-04-12 20:16:04 -0300

    Clase 7 en JavaScript

--- Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
+++ Javascript/leccion02/02-01-ConcatenacionDeCadenas.js
@@ -13,3 +13,22 @@ console.log(juntos);
 
 nombre += apellido; //Tercera Concatenacion usando el operador de suma
 console.log(nombre);
+
+//Hoy ya no se usa var, se utiliza let y const
+let nombre2 = 'Pedro';
+console.log(nombre2);
+
+const apellido2 = 'Lepes';
+//apellido2 = 'Peres'; una constante no puede ser modificada
+console.log(apellido2);
+
+let x, y; //Se puede crear varias variables dentro de una misma línea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de la misma línea
+let z = x + y; //Se asigna el valor de la operacion
+console.log(z);
+
+let _1num = 31;//No utilizar números para iniciar el nombre de la variable
+let rompiendo = "rompe"; //No utilizar palabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiendo);
---
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -0,0 +1,17 @@
+//Ejercicio para encontrar números pares
+let parImpar = 10;
+if(parImpar % 2 == 0){
+    console.log("Es un número PAR");
+}
+else{
+    console.log("Es un número IMPAR")
+}
+
+//Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if(edad >= adulto){
+    console.log("Usted es una persona mayor de edad");
+}
+else{
+    console.log("Usted es una persona menor de edad");
+}
