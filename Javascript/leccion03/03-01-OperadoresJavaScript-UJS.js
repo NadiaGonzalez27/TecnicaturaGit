b2fc19195d79ee637e2aea13955125781771e623
Nadia Gonzalez <nadiagonzalez206@gmail.com>
2023-04-13 17:10:32 -0300

    Ejercicios en js
    Calcular estacion del año, hora del dia

--- Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
+++ Javascript/leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -15,3 +15,54 @@ if(edad >= adulto){
 else{
     console.log("Usted es una persona menor de edad");
 }
+
+//Ejercicio: Dentro de un rango
+let dentroRango = 10;
+let valMin = 0, valMax = 10;
+if(dentroRango >= valMin && dentroRango <= valMax){
+    console.log("Esta dentro del rango establecido");
+}
+else{
+    console.log("Esta fuera del rango establecido");
+}
+
+//Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir al juego de su hijo");
+}
+else{
+    console.log("El padre No puede asistir al juego de su hijo");
+}
+
+//Operador Ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2);
+let numero = 12;
+resultado2 = numero % 2 == 0 ? "Es un número PAR" : "Es un número IMPAR";
+console.log(resultado2);
+
+//Convertir String a Number
+let miNumero = "21"; //Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); //Esta es una funcion
+console.log(typeof edad2);
+//Funcion isNaN
+if(isNaN(edad2)){ //No es un número = is Not a Number(devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros");
+}
+else{
+    if(edad2 >= 18){
+        console.log("Puede votar");
+    }
+    else{
+        console.log("Muy joven para votar");
+    }   
+}
+
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+console.log(resultado3);
+
+
--- Javascript/leccion04/04-01-Ejercicios.js
+++ Javascript/leccion04/04-01-Ejercicios.js
@@ -1,3 +1,71 @@
+//Ejercicio 1: Calcular estacion del año
+let mes = "9";
+let estacion; //Undefined
+if(mes == 1 || mes == 2 || mes == 12){
+    estacion = "Verano";
+}
+else if(mes == 3 || mes == 4 || mes == 5){
+    estacion = "Otoño";
+}
+else if(mes == 6 || mes == 7 || mes == 8){
+    estacion = "Invierno";
+}
+else if(mes == 9 || mes == 10 || mes == 11){
+    estacion = "Primavera";
+}
+else{
+    estacion = "Vaor incorrecto";
+}
+console.log("El valor corresponde a la estacion de: "+estacion);
+
+//Ejercicio2: Hora del dia
+let horaDia = 6;
+let hora;
+if(hora == 7 || hora == 8 ){ //if(horaDia >= 7 && horaDia <= 8)
+    hora = "Desayunando";
+}
+else if(horaDia == 9 || horaDia == 10 || horaDia == 11 || horaDia == 12){//if(horaDia >= 9 && horaDia <= 12)
+    hora = "Trabajando";
+}
+else if(horaDia == 13 || horaDia == 14){//if(horaDia >= 13 && horaDia <= 14)
+    hora = "Almorzando";
+}
+else if(horaDia == 15 ){
+    hora = " En un break";
+}
+else if(horaDia == 16 || horaDia == 17 || horaDia == 18 || horaDia == 19 || horaDia == 20){//if(horaDia >= 16 && horaDia <= 20)
+    hora = "Trabajando";
+}
+else if(horaDia == 21 || horaDia == 22){//if(horaDia >= 21 && horaDia <= 22)
+    hora = "Cenando";
+}
+else if(horaDia == 23 || horaDia == 24 || horaDia == 1 || horaDia == 2 || horaDia == 3 || horaDia == 4 || horaDia == 5 || horaDia == 6){//if(horaDia >= 23 && horaDia <= 6)
+    hora = "Durmiendo";
+}
+else{
+    hora = "Hora no encontrada";
+}
+console.log("Segun la hora del día estoy: "+hora);
+
+//Estructura switch(la sintaxis es igual a Java)
+switch(mes){ //No solo se pueden utilizar numero, tambien cadenas
+    case 1: case 2: case 12:
+    estacion = "Verano";
+    break;
+    case 3: case 4: case 5:
+    estacion = "Otoño";
+    break;
+    case 6: case 7: case 8:
+    estacion = "Invierno";
+    break;
+    case 9: case 10: case 11:
+    estacion = "Primavera";
+    break;
+    default:
+        estacion = "Valor incorrecto";
+}
+console.log("Bienvenidos a la estación de: "+estacion);
+
 //Ampliaremos el uso de var let y const
 /*
 Con var puedes reasignar en cualquier momento 
